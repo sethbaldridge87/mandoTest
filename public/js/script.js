56f1f6830d445bb5b22254a6670444b6d196bb32
sethbaldridge87 <sethbaldridge87@gmail.com>
2020-02-09 12:47:52 -0700

    Functionality complete

--- public/js/script.js
+++ public/js/script.js
@@ -12,16 +12,56 @@ $(document).ready(function(){
 	xmlhttp.send();
 	function testParse(data){
 		for (var i = 0; i < data.test.length; i++){
-			var slotStart = ('<div class="form-group"><h6>' + data.test[i].question + '</h6>');
 			var slotContent = [];
+			var totalVal = 0;
+			var slotStart;
 			for (var x = 0; x < data.test[i].answers.length; x++) {
-				slotContent.push('<div class="form-check"><input class="form-check-input" type="checkbox" data-correct="' + data.test[i].answers[x].correct + '"><label class="form-check-label">' + data.test[i].answers[x].text + '</label></div>')
+				if (data.test[i].type == 'single') {
+					// Radio Type
+					slotContent.push('<div class="form-check"><input class="form-check-input" id="' + data.test[i].answers[x].text + '" type="radio" name="' + data.test[i].question + '" data-correct="' + data.test[i].answers[x].correct + '"><label class="form-check-label" for="' + data.test[i].answers[x].text + '">' + data.test[i].answers[x].text + '</label></div>');
+					slotStart = ('<div class="form-group"><h6>' + data.test[i].question + '</h6>');
+				} else if (data.test[i].type == 'multiple') {
+					// Checkbox Type
+					if (data.test[i].answers[x].correct == true) {
+						totalVal++;
+					}
+					slotContent.push('<div class="form-check" data-length="' + data.test[i].answers.length + '"><input class="form-check-input checker" id="' + data.test[i].answers[x].text + '" type="checkbox" data-correct="' + data.test[i].answers[x].correct + '"><label class="form-check-label" for="' + data.test[i].answers[x].text + '">' + data.test[i].answers[x].text + '</label></div>');
+					slotStart = ('<div class="form-group" data-total="' + totalVal + '"><h6>' + data.test[i].question + ' (Select ' + totalVal + ' )</h6>');
+				}
 			};
 			var slotEnd = ('</div>');
 			var slotContent = slotContent.join(' ');
 			var slotLine = slotStart + slotContent + slotEnd;
-			console.log(slotLine);
 			$('#test-text').append(slotLine);
 		}
+		var slotButton = ('<input type="button" class="btn" value="Submit" />')
+		$('#test-text').append(slotButton);
+		// Selecting multiple answers
+		$('.form-check > .checker').on('change', function() {
+			var limit = $(this).closest('.form-group').attr('data-total');
+			if($(this).closest('.form-group').find('.checker:checked').length > limit) {
+				this.checked = false;
+			}
+		});
+		// Submit score
+		$('#test-text .btn').on('click', function(){
+			var grandTotal = 0;
+			var highestScore = 0;
+			$('.form-check-input').each(function(){
+				if ($(this).attr('data-correct') == 'true' && $(this).is(':checked')) {
+					grandTotal++
+				}
+				if ($(this).attr('data-correct') == 'true') {
+					highestScore++
+				}
+			});
+			grandTotal = grandTotal * 100 / highestScore;
+			grandTotal = grandTotal.toFixed(0);
+			$('#final-score h2').html('Your final score is: ' + grandTotal + '%');
+			$('#reset').html('<button>Try Again?</button>');
+			$('#reset button').on('click', function(){
+				window.location.reload();
+			});
+		});
 	}
 });
