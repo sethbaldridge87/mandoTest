2e671ae006639969e82b4bdd4adb3c221765361f
sethbaldridge87 <sethbaldridge87@gmail.com>
2020-02-09 15:19:43 -0700

    All questions answered

--- public/js/script.js
+++ public/js/script.js
@@ -11,7 +11,9 @@ $(document).ready(function(){
 	xmlhttp.open("GET",location,true);
 	xmlhttp.send();
 	function testParse(data){
+		var counter = 0;
 		for (var i = 0; i < data.test.length; i++){
+			counter++;
 			var slotContent = [];
 			var totalVal = 0;
 			var slotStart;
@@ -19,17 +21,17 @@ $(document).ready(function(){
 				if (data.test[i].type == 'single') {
 					// Radio Type
 					slotContent.push('<div class="form-check"><input class="form-check-input" id="' + data.test[i].answers[x].text + '" type="radio" name="' + data.test[i].question + '" data-correct="' + data.test[i].answers[x].correct + '"><label class="form-check-label" for="' + data.test[i].answers[x].text + '">' + data.test[i].answers[x].text + '</label></div>');
-					slotStart = ('<div class="form-group"><h6>' + data.test[i].question + '</h6>');
+					slotStart = ('<div class="form-group"><h6>#' + counter + ' ' + data.test[i].question + '</h6>');
 				} else if (data.test[i].type == 'multiple') {
 					// Checkbox Type
 					if (data.test[i].answers[x].correct == true) {
 						totalVal++;
 					}
 					slotContent.push('<div class="form-check" data-length="' + data.test[i].answers.length + '"><input class="form-check-input checker" id="' + data.test[i].answers[x].text + '" type="checkbox" data-correct="' + data.test[i].answers[x].correct + '"><label class="form-check-label" for="' + data.test[i].answers[x].text + '">' + data.test[i].answers[x].text + '</label></div>');
-					slotStart = ('<div class="form-group" data-total="' + totalVal + '"><h6>' + data.test[i].question + ' (Select ' + totalVal + ' )</h6>');
+					slotStart = ('<div class="form-group" data-total="' + totalVal + '"><h6>#' + counter + '. ' + data.test[i].question + ' (Select ' + totalVal + ')</h6>');
 				}
 			};
-			var slotEnd = ('</div>');
+			var slotEnd = ('</div><br>');
 			var slotContent = slotContent.join(' ');
 			var slotLine = slotStart + slotContent + slotEnd;
 			$('#test-text').append(slotLine);
@@ -45,6 +47,8 @@ $(document).ready(function(){
 		});
 		// Submit score
 		$('#test-text .btn').on('click', function(){
+			$('[data-correct=true]').closest('.form-check').css('color','green');
+			$('[data-correct=false]').closest('.form-check').css('color','red');
 			var grandTotal = 0;
 			var highestScore = 0;
 			$('.form-check-input').each(function(){
@@ -58,7 +62,7 @@ $(document).ready(function(){
 			grandTotal = grandTotal * 100 / highestScore;
 			grandTotal = grandTotal.toFixed(0);
 			$('#final-score h2').html('Your final score is: ' + grandTotal + '%');
-			$('#reset').html('<button>Try Again?</button>');
+			$('#reset').html('<br><button class="btn">Try Again?</button>');
 			$('#reset button').on('click', function(){
 				window.location.reload();
 			});
